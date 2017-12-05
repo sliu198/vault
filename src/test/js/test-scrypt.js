02073c3009cd0333dd22aa4bbc4ff03d7ccbc199
Solomon Liu <sliu198@gmail.com>
2017-12-05 08:45:02 -0800

    use sjcl to test speed

--- src/test/js/test-scrypt.js
+++ src/test/js/test-scrypt.js
@@ -1,5 +1,7 @@
 "use strict";
 let assert = require('assert');
+global.sjcl = require('sjcl');
+require('sjcl/core/scrypt');
 let scrypt = require('../../main/js/scrypt');
 
 describe("scrypt",function() {
@@ -114,5 +116,33 @@ describe("scrypt",function() {
         //     "37304049e8a952fbcbf45c6fa77a41a4"
         // );
 
-    }, 10);
+    });
+
+    it('sjcl', function() {
+        this.timeout(0);
+
+        assert.strictEqual(
+            sjcl.misc.scrypt("","",16, 1, 1, 64 * 8).map(function(o){return (o < 0 ? o + 0x100000000 : o).toString(16).padStart(8,'0')}).join(''),
+            "77d6576238657b203b19ca42c18a0497" +
+            "f16b4844e3074ae8dfdffa3fede21442" +
+            "fcd0069ded0948f8326a753a0fc81f17" +
+            "e8d3e0fb2e0d3628cf35e20c38d18906"
+        );
+
+        assert.strictEqual(
+            sjcl.misc.scrypt("password","NaCl",1024, 8, 16, 64 * 8).map(function(o){return (o < 0 ? o + 0x100000000 : o).toString(16).padStart(8,'0')}).join(''),
+            "fdbabe1c9d3472007856e7190d01e9fe" +
+            "7c6ad7cbc8237830e77376634b373162" +
+            "2eaf30d92e22a3886ff109279d9830da" +
+            "c727afb94a83ee6d8360cbdfa2cc0640"
+        );
+
+        assert.strictEqual(
+            sjcl.misc.scrypt("pleaseletmein","SodiumChloride",16384, 8, 1, 64 * 8).map(function(o){return (o < 0 ? o + 0x100000000 : o).toString(16).padStart(8,'0')}).join(''),
+            "7023bdcb3afd7348461c06cd81fd38eb" +
+            "fda8fbba904f8e3ea9b543f6545da1f2" +
+            "d5432955613f0fcf62d49705242a9af9" +
+            "e61e85dc0d651e40dfcf017b45575887"
+        );
+    });
 });
