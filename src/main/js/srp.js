e2095c21a2d246edda0e18e7d11a8d0ee0cbf062
Solomon Liu <sliu198@gmail.com>
2017-12-07 11:18:29 -0800

    srp functions

--- src/main/js/srp.js
+++ src/main/js/srp.js
@@ -1,3 +1,112 @@
 "use strict";
-let bmath = require('./buffer-math');
-let crypto = require('crypto');
+global.sjcl = require('sjcl');
+require('sjcl/core/scrypt');
+require('sjcl/core/bn');
+require('sjcl/core/srp');
+
+let makePublicB = function(v, secretB, group, hash) {
+    let groupParams = getGroupParams(group);
+    let k = getK(group,hash);
+    let N = groupParams.N;
+    return k.mulmod(v,N).add(groupParams.g.powermod(secretB, N)).mod(N);
+};
+
+let makeServerS = function(v, publicA, secretB, group, hash) {
+    let groupParams = getGroupParams(group);
+    let N = groupParams.N;
+    if (publicA.mod(N).equals(new sjcl.bn(0))) {
+        throw new Error("publicA cannot be 0");
+    }
+    let publicB = makePublicB(v,secretB,group,hash);
+    let u = makeU(publicA,publicB,group, hash);
+    return publicA.mulmod(v.powermod(u,N),N).powermod(secretB,N);
+};
+
+let makeServerKey = function(v, publicA, secretB, group, hash) {
+    return makeKey(makeServerS(v,publicA,secretB,group,hash), group, hash);
+};
+
+let makeV = function(x,group) {
+    let groupParams = getGroupParams(group);
+    return groupParams.g.powermod(x,groupParams.N);
+};
+
+let makePublicA = function(secretA,group) {
+    let groupParams = getGroupParams(group);
+    let publicA = groupParams.g.powermod(secretA,groupParams.N);
+    if (publicA.equals(new sjcl.bn(0))) {
+        throw new Error("publicA cannot be 0; Try another value for secretA");
+    }
+    return publicA;
+};
+
+let makeClientS = function(x, secretA, publicB, group, hash) {
+    let groupParams = getGroupParams(group);
+    let k = getK(group,hash);
+    let publicA = makePublicA(secretA,group);
+    let u = makeU(publicA,publicB,group,hash);
+    let N = groupParams.N;
+    return publicB.sub(k.mulmod(groupParams.g.powermod(x,N),N)).powermod(secretA.add(u.mulmod(x,N)),N);
+};
+
+let makeClientKey = function(x,secretA,publicB,group,hash) {
+    makeKey(makeClientS(x,secretA,publicB,group,hash),group,hash)
+};
+
+let getGroupParams = function(group) {
+    let groupParams = sjcl.keyexchange.srp.knownGroup(group);
+    if (!groupParams) {
+        throw new Error("Unknown SRP group");
+    }
+    return groupParams;
+};
+
+let padArray = function(a, length) {
+    let bitLength = sjcl.bitArray.bitLength(a);
+    if (bitLength >= length) {
+        return a;
+    }
+    length -= bitLength;
+    let padding = new Array(Math.floor(length / 32)).fill(0);
+    if (length % 32) {
+        padding = sjcl.bitArray.concat(padding,sjcl.bitArray.partial(length % 32, 0));
+    }
+    return sjcl.bitArray.concat(padding,a);
+};
+
+let getK = function(group, hash) {
+    hash = new (hash || sjcl.hash.sha256)();
+    let groupParams = getGroupParams(group);
+    hash.update(groupParams.N.toBits()); //no need to pad because by definition bitLength(N) === group
+    hash.update(padArray(groupParams.g.toBits(), group));
+    return sjcl.bn.fromBits(hash.finalize());
+};
+
+let makeU = function(publicA,publicB,group,hash) {
+    hash = new (hash || sjcl.hash.sha256)();
+    hash.update(padArray(publicA.toBits(), group));
+    hash.update(padArray(publicB.toBits(), group));
+    return sjcl.bn.fromBits(hash.finalize());
+};
+
+let generateSecret = function(group) {
+    return sjcl.bn.random(getGroupParams(group).N);
+};
+
+let makeKey = function(S,group,hash) {
+    hash = new (hash || sjcl.hash.sha256)();
+    hash.update(padArray(S.toBits(),group));
+    return hash.finalize();
+};
+
+exports.makeV = makeV;
+exports.getK = getK;
+exports.generateSecretA = generateSecret;
+exports.makePublicA = makePublicA;
+exports.generateSecretB = generateSecret;
+exports.makePublicB = makePublicB;
+exports.makeU = makeU;
+exports.makeClientS = makeClientS;
+exports.makeServerS = makeServerS;
+exports.makeClientKey = makeClientKey;
+exports.makeServerKey = makeServerKey;
---
+++ src/test/js/test-srp.js
@@ -0,0 +1,91 @@
+let assert = require('assert');
+let srp = require('../../main/js/srp');
+require('sjcl/core/sha1');
+
+const k = "7556AA045AEF2CDD07ABAF0F665C3E818913186F";
+const A =
+    "61D5E490F6F1B79547B0704C436F523DD0E560F0C64115BB72557EC4" +
+    "4352E8903211C04692272D8B2D1A5358A2CF1B6E0BFCF99F921530EC" +
+    "8E39356179EAE45E42BA92AEACED825171E1E8B9AF6D9C03E1327F44" +
+    "BE087EF06530E69F66615261EEF54073CA11CF5858F0EDFDFE15EFEA" +
+    "B349EF5D76988A3672FAC47B0769447B";
+const B =
+    "BD0C61512C692C0CB6D041FA01BB152D4916A1E77AF46AE105393011" +
+    "BAF38964DC46A0670DD125B95A981652236F99D9B681CBF87837EC99" +
+    "6C6DA04453728610D0C6DDB58B318885D7D82C7F8DEB75CE7BD4FBAA" +
+    "37089E6F9C6059F388838E7A00030B331EB76840910440B1B27AAEAE" +
+    "EB4012B7D7665238A8E3FB004B117B58";
+const u = "CE38B9593487DA98554ED47D70A7AE5F462EF019";
+const x = "94B7555AABE9127CC58CCF4993DB6CF84D16C124";
+const v =
+    "7E273DE8696FFC4F4E337D05B4B375BEB0DDE1569E8FA00A9886D812" +
+    "9BADA1F1822223CA1A605B530E379BA4729FDC59F105B4787E5186F5" +
+    "C671085A1447B52A48CF1970B4FB6F8400BBF4CEBFBB168152E08AB5" +
+    "EA53D15C1AFF87B2B9DA6E04E058AD51CC72BFC9033B564E26480D78" +
+    "E955A5E29E7AB245DB2BE315E2099AFB";
+const a = "60975527035CF2AD1989806F0407210BC81EDC04E2762A56AFD529DDDA2D4393";
+const b = "E487CB59D31AC550471E81F00F6928E01DDA08E974A004F49E61F5D105284D20";
+const S =
+    "B0DC82BABCF30674AE450C0287745E7990A3381F63B387AAF271A10D" +
+    "233861E359B48220F7C4693C9AE12B0A6F67809F0876E2D013800D6C" +
+    "41BB59B6D5979B5C00A172B4A2A5903A0BDCAF8A709585EB2AFAFA8F" +
+    "3499B200210DCC1F10EB33943CD67FC88A2F39A4BE5BEC4EC0A3212D" +
+    "C346D7E474B29EDE8A469FFECA686E5A";
+
+describe('srp', function() {
+    it('getK', function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.getK(1024, sjcl.hash.sha1).toBits()).toUpperCase(),
+            k
+        );
+    });
+
+    it('makeU', function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makeU(new sjcl.bn(A), new sjcl.bn(B), 1024, sjcl.hash.sha1).toBits()).toUpperCase(),
+            u
+        );
+    });
+
+    it('makeV', function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makeV(new sjcl.bn(x), 1024).toBits()).toUpperCase(),
+            v
+        );
+    });
+
+    it('makeA', function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makePublicA(new sjcl.bn(a), 1024).toBits()).toUpperCase(),
+            A
+        );
+    });
+
+    it('makeClientS',function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makeClientS(new sjcl.bn(x), new sjcl.bn(a), new sjcl.bn(B), 1024, sjcl.hash.sha1).toBits()).toUpperCase(),
+            S
+        );
+    });
+
+    it('makeB', function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makePublicB(new sjcl.bn(v),new sjcl.bn(b),1024,sjcl.hash.sha1).toBits()).toUpperCase(),
+            B
+        );
+    });
+
+    it('makeServerS',function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makeServerS(new sjcl.bn(v), new sjcl.bn(A), new sjcl.bn(b), 1024, sjcl.hash.sha1).toBits()).toUpperCase(),
+            S
+        );
+    });
+
+    it('matching S', function() {
+        assert.strictEqual(
+            sjcl.codec.hex.fromBits(srp.makeServerS(new sjcl.bn(v), new sjcl.bn(A), new sjcl.bn(b), 1024, sjcl.hash.sha1).toBits()).toUpperCase(),
+            sjcl.codec.hex.fromBits(srp.makeClientS(new sjcl.bn(x), new sjcl.bn(a), new sjcl.bn(B), 1024, sjcl.hash.sha1).toBits()).toUpperCase()
+        );
+    });
+});
